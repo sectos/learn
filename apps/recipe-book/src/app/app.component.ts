3ae2e054f48f78872fa6c924ca802d8ae1735342
Jared Gomes <limitless.software.development@gmail.com>
2022-10-06 21:09:25 -0400

    Initial commit

---
+++ apps/recipe-book/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'learn-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'recipe-book';
+}
