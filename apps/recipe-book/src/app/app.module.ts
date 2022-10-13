336aae690509ff02e0fd2492284d3a6741341c39
Jared Gomes <limitless.software.development@gmail.com>
2022-10-13 16:20:00 -0400

    remove old files

--- apps/recipe-book/src/app/app.module.ts
+++ apps/recipe-book/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ServersModule } from '@learn/servers';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ServersModule],
+  imports: [BrowserModule],
   providers: [],
   /**
    * ? The `bootstrap` property should only every be used in the root module of our application. This tells angular what components are required to bootstrap our application.
